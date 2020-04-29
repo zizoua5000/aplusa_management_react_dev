cfeb04cb2c0228a43bf58d809aee186e28a96e1a
asadmammadov <asadmammadov@gmail.com>
2020-04-29 10:37:58 +0400

    mark api updates implemented to model

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     // withCredentials: true,
      baseURL: 'http://192.168.20.142:8010/api/',
-    //baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'http://127.0.0.1:8000/api/',
     // headers: {
     //     "API-KEY": "+*=#fk6hal!1g=97b%(2obmvq&&9l-h4rprwsq#1g5()hodm@j"
     // }
@@ -47,7 +47,7 @@ export const vehicleMarkAPI = {
 }
 
 export const simcardAPI = {
-    getsimcard() {
+    getSimcard() {
         return instance.get(`simcard/list_create/`)
             .then(response => {
                 return response.data;
@@ -60,7 +60,7 @@ export const simcardAPI = {
 
 
 export const vehicleTypeAPI = {
-    getvehicleType() {       
+    getVehicleType() {       
         return instance.get('vehicle_type/list_create/')
             .then(response => {
                 return response.data;
@@ -108,7 +108,7 @@ export const vehicleModelAPI = {
 
 }
 export const jobTitleAPI = {
-    getjobTitle() {
+    getJobTitle() {
         return instance.get(`job_title/list_create/`)
             .then(response => {
                 return response.data;
