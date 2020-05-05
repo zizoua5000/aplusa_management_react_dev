846076925088ebcd6c7178cd53da12c11afffa10
Nicat Talibov <nijat.t@aplusa-security.com>
2020-05-05 16:02:57 +0400

    paginator update

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // withCredentials: true,
-     baseURL: 'http://192.168.20.142:8010/api/',
-    // baseURL: 'http://127.0.0.1:8000/api/',
+    //  baseURL: 'http://192.168.20.142:8010/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
     // headers: {
     //     "API-KEY": "+*=#fk6hal!1g=97b%(2obmvq&&9l-h4rprwsq#1g5()hodm@j"
     // }
--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -14,8 +14,9 @@ let Paginator = ({totalItemsCount, pageSize,
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
+    let initialPortionNumber=Math.ceil(currentPage / portionSize);
     let portionCount = Math.ceil(pagesCount / portionSize);
-    let [portionNumber, setPortionNumber] = useState(1);
+    let [portionNumber, setPortionNumber] = useState(initialPortionNumber);
    
     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
     let rightPortionPageNumber = portionNumber * portionSize;
