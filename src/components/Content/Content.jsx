b9d08be7d1967ce007e9a8492706dad1608cf2fa
asadmammadov <asadmammadov@gmail.com>
2020-07-01 11:30:57 +0400

    .

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Footer from '../Footer/Footer'
 
 import VehicleContainer from '../Vehicle/VehicleContainer'
 import VehicleCreateContainer from '../Vehicle/VehicleCreateContainer'
@@ -12,6 +14,11 @@ import VehicleTypeContainer from '../VehicleType/VehicleTypeContainer'
 import VehicleTypeCreateContainer from '../VehicleType/VehicleTypeCreateContainer'
 import VehicleTypeUpdateContainer from '../VehicleType/VehicleTypeUpdateContainer'
 
+import VehicleModelContainer from '../VehicleModel/VehicleModelContainer'
+import VehicleModelCreateContainer from '../VehicleModel/VehicleModelCreateContainer'
+import VehicleModelUpdateContainer from '../VehicleModel/VehicleModelUpdateContainer'
+
+
 import ProjectContainer from '../Project/ProjectContainer'
 import ProjectCreateContainer from '../Project/ProjectCreateContainer'
 import ProjectUpdateContainer from '../Project/ProjectUpdateContainer'
@@ -24,9 +31,9 @@ import StatusContainer from '../Status/StatusContainer'
 import StatusCreateContainer from '../Status/StatusCreateContainer'
 import StatusUpdateContainer from '../Status/StatusUpdateContainer'
 
-import VehicleModelContainer from '../VehicleModel/VehicleModelContainer'
-import VehicleModelCreateContainer from '../VehicleModel/VehicleModelCreateContainer'
-import VehicleModelUpdateContainer from '../VehicleModel/VehicleModelUpdateContainer'
+import DepartmentContainer from '../Department/DepartmentContainer'
+import DepartmentCreateContainer from '../Department/DepartmentCreateContainer'
+import DepartmentUpdateContainer from '../Department/DepartmentUpdateContainer'
 
 import JobTitleContainer from '../JobTitle/JobTitleContainer'
 import JobTitleCreateContainer from '../JobTitle/JobTitleCreateContainer'
@@ -36,38 +43,46 @@ import SimcardContainer from '../Simcard/SimcardContainer'
 import SimcardCreateContainer from '../Simcard/SimcardCreateContainer'
 import SimcardUpdateContainer from '../Simcard/SimcardUpdateContainer'
 
+
 import CompanyTypeContainer from '../CompanyType/CompanyTypeContainer'
 import CompanyTypeCreateContainer from '../CompanyType/CompanyTypeCreateContainer'
 import CompanyTypeUpdateContainer from '../CompanyType/CompanyTypeUpdateContainer'
+
 import CompanyContainer from '../Company/CompanyContainer'
 import CompanyCreateContainer from '../Company/CompanyCreateContainer'
 import CompanyUpdateContainer from '../Company/CompanyUpdateContainer'
+
+
 import DeviceTypeContainer from '../DeviceType/DeviceTypeContainer'
 import DeviceTypeCreateContainer from '../DeviceType/DeviceTypeCreateContainer'
 import DeviceTypeUpdateContainer from '../DeviceType/DeviceTypeUpdateContainer'
+
 import DeviceMarkContainer from '../DeviceMark/DeviceMarkContainer'
 import DeviceMarkCreateContainer from '../DeviceMark/DeviceMarkCreateContainer'
 import DeviceMarkUpdateContainer from '../DeviceMark/DeviceMarkUpdateContainer'
-import DepartmentContainer from '../Department/DepartmentContainer'
-import DepartmentCreateContainer from '../Department/DepartmentCreateContainer'
-import DepartmentUpdateContainer from '../Department/DepartmentUpdateContainer'
+
 import DeviceModelContainer from '../DeviceModel/DeviceModelContainer'
 import DeviceModelCreateContainer from '../DeviceModel/DeviceModelCreateContainer'
 import DeviceModelUpdateContainer from '../DeviceModel/DeviceModelUpdateContainer'
+
 import DeviceContainer from '../Device/DeviceContainer'
 import DeviceCreateContainer from '../Device/DeviceCreateContainer'
 import DeviceUpdateContainer from '../Device/DeviceUpdateContainer'
+
 import DeviceLocationContainer from '../DeviceLocation/DeviceLocationContainer'
 import DeviceLocationCreateContainer from '../DeviceLocation/DeviceLocationCreateContainer'
 import DeviceLocationUpdateContainer from '../DeviceLocation/DeviceLocationUpdateContainer'
+
+
 import ConfigurationContainer from '../Configuration/ConfigurationContainer'
 import ConfigurationCreateContainer from '../Configuration/ConfigurationCreateContainer'
 import ConfigurationUpdateContainer from '../Configuration/ConfigurationUpdateContainer'
+
 import UserContainer from '../User/UserContainer'
 import UserCreateContainer from '../User/UserCreateContainer'
 import UserUpdateContainer from '../User/UserUpdateContainer'
-import { Route } from 'react-router-dom';
-import Footer from '../Footer/Footer'
+
+
 
 
 class Content extends React.Component {
@@ -89,10 +104,18 @@ class Content extends React.Component {
                         <Route path='/vehicle_type_create' render={() => <VehicleTypeCreateContainer />}></Route>
                         <Route path='/vehicle_type_update/:id?' render={() => <VehicleTypeUpdateContainer />}></Route>
 
+                        <Route path='/vehicle_model/:pageNumber?' render={() => <VehicleModelContainer />}></Route>
+                        <Route path='/vehicle_model_create' render={() => <VehicleModelCreateContainer />}></Route>
+                        <Route path='/vehicle_model_update/:id?' render={() => <VehicleModelUpdateContainer />}></Route>                        
+
                         <Route path='/job_title' render={() => <JobTitleContainer />}></Route>                      
                         <Route path='/job_title_create' render={() => <JobTitleCreateContainer />}></Route>
                         <Route path='/job_title_update/:id?' render={() => <JobTitleUpdateContainer />}></Route>
-                        
+
+                    	<Route path='/department' render={() => <DepartmentContainer />}></Route>  
+                        <Route path='/department_create' render={() => <DepartmentCreateContainer />}></Route>
+                        <Route path='/department_update/:id?' render={() => <DepartmentUpdateContainer />}></Route>
+
                         <Route path='/region' render={() => <RegionContainer />}></Route>                      
                         <Route path='/region_create' render={() => <RegionCreateContainer />}></Route>
                         <Route path='/region_update/:id?' render={() => <RegionUpdateContainer />}></Route>
@@ -105,10 +128,6 @@ class Content extends React.Component {
                         <Route path='/project_create' render={() => <ProjectCreateContainer />}></Route>
                         <Route path='/project_update/:id?' render={() => <ProjectUpdateContainer />}></Route>
                         
-                        <Route path='/vehicle_model/:pageNumber?' render={() => <VehicleModelContainer />}></Route>
-                        <Route path='/vehicle_model_create' render={() => <VehicleModelCreateContainer />}></Route>
-                        <Route path='/vehicle_model_update/:id?' render={() => <VehicleModelUpdateContainer />}></Route>
-                        
                         <Route path='/simcard' render={() => <SimcardContainer />}></Route>
                         <Route path='/simcard_create' render={() => <SimcardCreateContainer />}></Route>
                         <Route path='/simcard_update/:id?' render={() => <SimcardUpdateContainer />}></Route>
@@ -116,30 +135,35 @@ class Content extends React.Component {
                         <Route path='/company_type' render={() => <CompanyTypeContainer />}></Route>  
                         <Route path='/company_type_create' render={() => <CompanyTypeCreateContainer />}></Route>
                         <Route path='/company_type_update/:id?' render={() => <CompanyTypeUpdateContainer />}></Route>
+
                         <Route path='/company' render={() => <CompanyContainer />}></Route>  
                         <Route path='/company_create' render={() => <CompanyCreateContainer />}></Route>
                         <Route path='/company_update/:id?' render={() => <CompanyUpdateContainer />}></Route>
+
                         <Route path='/device_type' render={() => <DeviceTypeContainer />}></Route>
                         <Route path='/device_type_create' render={() => <DeviceTypeCreateContainer />}></Route>
                         <Route path='/device_type_update/:id?' render={() => <DeviceTypeUpdateContainer />}></Route>
+
                         <Route path='/device_mark' render={() => <DeviceMarkContainer />}></Route>
                         <Route path='/device_mark_create' render={() => <DeviceMarkCreateContainer />}></Route>
-                        <Route path='/device_mark_update/:id?' render={() => <DeviceMarkUpdateContainer />}></Route>                        
-                    	<Route path='/department' render={() => <DepartmentContainer />}></Route>  
-                        <Route path='/department_create' render={() => <DepartmentCreateContainer />}></Route>
-                        <Route path='/department_update/:id?' render={() => <DepartmentUpdateContainer />}></Route>
+                        <Route path='/device_mark_update/:id?' render={() => <DeviceMarkUpdateContainer />}></Route>    
+
                         <Route path='/device_model' render={() => <DeviceModelContainer />}></Route>
                         <Route path='/device_model_create' render={() => <DeviceModelCreateContainer />}></Route>
                         <Route path='/device_model_update/:id?' render={() => <DeviceModelUpdateContainer />}></Route>
+
                         <Route path='/device' render={() => <DeviceContainer />}></Route>
                         <Route path='/device_create' render={() => <DeviceCreateContainer />}></Route>
                         <Route path='/device_update/:id?' render={() => <DeviceUpdateContainer />}></Route> 
+
                         <Route path='/device_location' render={() => <DeviceLocationContainer />}></Route>
                         <Route path='/device_location_create' render={() => <DeviceLocationCreateContainer />}></Route>
-                        <Route path='/device_location_update/:id?' render={() => <DeviceLocationUpdateContainer />}></Route>                                               
+                        <Route path='/device_location_update/:id?' render={() => <DeviceLocationUpdateContainer />}></Route> 
+
                         <Route path='/configuration' render={() => <ConfigurationContainer />}></Route>
                         <Route path='/configuration_create' render={() => <ConfigurationCreateContainer />}></Route>
-                        <Route path='/configuration_update/:id?' render={() => <ConfigurationUpdateContainer />}></Route>                                                 
+                        <Route path='/configuration_update/:id?' render={() => <ConfigurationUpdateContainer />}></Route>    
+                                                                     
                         <Route path='/user' render={() => <UserContainer />}></Route>
                         <Route path='/user_create' render={() => <UserCreateContainer />}></Route>
                         <Route path='/user_update/:id?' render={() => <UserUpdateContainer />}></Route> 
